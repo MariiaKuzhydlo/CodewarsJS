a399274e91d62b9055d1609c5da52f281cac0fd5
Mariia Kuzhydlo <kushnirmariia.m@gmail.com>
2022-12-04 23:55:22 +0200

    tasks added

---
+++ script.js
@@ -0,0 +1,17 @@
+// 1. Convert a Number to a String!
+
+function numberToString(num) {
+	let s = String(num);
+	return s;
+}
+
+
+// 2. Even or Odd
+
+function even_or_odd(number) {
+	if (number % 2 == 0){
+	   console.log( number = "Even");
+	} else {
+	   console.log( number = "Odd");
+	} return number; 
+} ;
